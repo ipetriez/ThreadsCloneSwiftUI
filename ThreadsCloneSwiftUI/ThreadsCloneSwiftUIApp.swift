173e5e18804ac24d093b9654fd790542d9d2fa51
Sergey Petrosyan <ipetriez@icloud.com>
2024-01-20 11:27:13 +0400

    Initial Commit

---
+++ ThreadsCloneSwiftUI/ThreadsCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneSwiftUIApp.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Sergey Petrosyan on 20.01.24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

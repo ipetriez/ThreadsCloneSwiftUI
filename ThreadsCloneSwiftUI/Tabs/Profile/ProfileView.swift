8cba0403483aaef9e2a1728744634db415471a93
Sergey Petrosyan <ipetriez@icloud.com>
2024-01-25 17:34:21 +0400

    Create ProfileView. Build wireframe for header inside it.

---
+++ ThreadsCloneSwiftUI/Tabs/Profile/ProfileView.swift
@@ -0,0 +1,56 @@
+//
+//  ProfileView.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Sergey Petrosyan on 25.01.24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 20) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Charlie Moody")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            
+                            Text("charlie_moody")
+                                .font(.subheadline)
+                        }
+                        
+                        Text("Some random Charlie")
+                            .font(.footnote)
+                        
+                        Text("13 followers")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }
+                    
+                    Spacer()
+                    
+                    AvatarImageView("av-3")
+                }
+                
+                Button(action: {}, label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 352, height: 32)
+                        .background(.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                })
+            }
+        }
+        .padding(.horizontal)
+        .scrollIndicators(.hidden)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ ThreadsCloneSwiftUI/Tabs/Profile/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Sergey Petrosyan on 25.01.24.
+//
+
+import Foundation

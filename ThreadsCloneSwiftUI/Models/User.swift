ff0f50f879b7b4efb853550e81487c9116581e21
Sergey Petrosyan <ipetriez@icloud.com>
2024-01-28 16:05:12 +0400

    Create User model and UserService.

---
+++ ThreadsCloneSwiftUI/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Sergey Petrosyan on 28.01.24.
+//
+
+import Foundation
+
+struct User: Codable, Identifiable {
+    let id: String
+    let fullName: String
+    let email: String
+    let userName: String
+    var profileImageURL: String?
+    var bio: String?
+}
---
+++ ThreadsCloneSwiftUI/Services/UserService.swift
@@ -0,0 +1,25 @@
+//
+//  UserService.swift
+//  ThreadsCloneSwiftUI
+//
+//  Created by Sergey Petrosyan on 28.01.24.
+//
+
+import Firebase
+import FirebaseFirestoreSwift
+
+final class UserService {
+    @Published var currentUser: User?
+    
+    static let shared = UserService()
+    
+    private init() {}
+    
+    @MainActor
+    func getCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let documentSnapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try documentSnapshot.data(as: User.self)
+        currentUser = user
+    }
+}
